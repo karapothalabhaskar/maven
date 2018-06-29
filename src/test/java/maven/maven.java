3255f08185bc9dc3e9a77bcc8b2540463cb8c3a9
ITWINE-QTP <ITWINE-QTP@bhaskar-itwine-PC>
2018-06-29 17:19:01 +0530

    changes

---
+++ src/test/java/maven/maven.java
@@ -0,0 +1,38 @@
+package maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class maven 
+{
+	WebDriver driver;
+	@BeforeTest
+	public void login() throws Exception
+	{
+		System.out.println("hello GWG Application");
+		System.setProperty("webdriver.gecko.driver","D:/selenium Projects/geckodriver/V0.15.0/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://13.76.131.82/DevGwg_HHE/Account/Login.aspx");
+		driver.findElement(By.xpath("//*[@id=\"MainContent_UserName1\"]")).sendKeys("guest");
+		driver.findElement(By.xpath("//*[@id=\"MainContent_Password1\"]")).sendKeys("amtote");
+		driver.findElement(By.xpath("//*[@id=\"MainContent_LoginButton\"]")).click();
+		Thread.sleep(1000);	
+	}
+	@Test
+	public void AddUser()
+	{
+		System.out.println("we are creating the user here");
+	}
+	@AfterTest
+	public void DeleteUser()
+	{
+		System.out.println("we are deleting the created user and exit form the application");
+		System.out.println("coming out of GWG Application");
+	}
+
+}
---
+++ src/test/java/maven/yes2treatment.java
@@ -0,0 +1,41 @@
+package maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class yes2treatment 
+{
+	
+	WebDriver driver;
+	@BeforeTest
+	public void Login() throws Exception
+	{
+		System.out.println("hello we are in yes2treatment application");
+		System.setProperty("webdriver.gecko.driver","D:/selenium Projects/geckodriver/V0.15.0/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://202.83.19.104/yes2treatment/index/home");
+		driver.findElement(By.xpath("//*[@id=\"navigation-example\"]/ul/li[5]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("admin@yes2treatment.com");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("1234");
+		driver.findElement(By.xpath("//*[@id=\"loginbtn\"]")).click();		
+	}
+	@Test
+	public void AddUser()
+	{
+		System.out.println("we are creating the user here");
+	}
+	@AfterTest
+	public void DeleteUser()
+	{
+		System.out.println("we are deleting the created user and exit form the application");
+		driver.quit();
+		System.out.println("coming out of yes2treatment Application");
+	}
+
+}
